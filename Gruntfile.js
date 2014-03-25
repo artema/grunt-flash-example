f7447e40d6efd484fa19af4ac4232ed199cd89c4
Artem Abashev <artem@abashev.com>
2014-03-25 20:39:24 +0400

    Example Flash project and Grunt script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,44 @@
+'use strict';
+
 module.exports = function(grunt) {
-	
+
+    grunt.initConfig({
+        compc: {
+            options: {
+            },
+            compc: {
+                src: ['flash/lib/src/**/*.as', 'lib/src/**/*.mxml'],
+                dest: 'flash/lib/bin/example-lib.swc',
+                options: {
+                    'source-path': ['flash/lib/src/'] 
+                }
+            }
+        },
+
+        mxmlc: {
+            options: {
+                rawConfig: '-library-path+=flash/lib/bin'
+            },
+            mxmlc: {
+                files: {
+                    'flash/app/bin-release/app.swf': ['flash/app/src/**/*.as', 'flash/app/src/**/*.mxml']
+                }
+            }
+        },
+        
+        clean: {
+            options: {
+                force: true  
+            },
+            compc: ['flash/lib/bin/*.*', 'flash/app/bin-debug/*.*', 'flash/app/bin-release/*.*'],
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-mxmlc');
+    grunt.loadNpmTasks('grunt-compc');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    
+    grunt.registerTask('test', ['clean', 'compc', 'mxmlc', 'clean']);
+
+    grunt.registerTask('default', ['test']);
 };
---
+++ flash/lib/src/com/example/MyClass.as
@@ -0,0 +1,10 @@
+package com.example
+{
+	public class MyClass
+	{
+		public function get message():String
+		{
+			return "Hello World";
+		}
+	}
+}
